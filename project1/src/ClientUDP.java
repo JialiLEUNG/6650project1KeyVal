6ee8304da680d586fffee31d29cfcdd8843cd481
Jiali <hcloud@Jianyuns-MacBook-Air.local>
2020-01-30 17:09:46 -0800

    update udp

--- project1/src/ClientUDP.java
+++ project1/src/ClientUDP.java
@@ -1,6 +1,3 @@
-import java.io.IOException;
-import java.net.*;
-
 /**
  * ClientUDP demonstrates how to implement a UDP client program
  * that requests an echo service.
@@ -9,42 +6,65 @@ import java.net.*;
  * java ClientUDP.java 127.0.0.1 32000
  *
  */
+
+import java.io.IOException;
+import java.net.*;
+import java.util.Scanner;
+
+
 public class ClientUDP {
-    public static void main(String[] args){
-        if (args.length < 2){
+    private DatagramSocket socket;
+    private InetAddress address;
+    private int port;
+    private Scanner clientScanner;
+
+    // ClientUDP constructor
+    private ClientUDP(String address, int port) throws IOException{
+        this.address = InetAddress.getByName(address);
+        this.port = port;
+        socket = new DatagramSocket(this.port);
+        clientScanner = new Scanner(System.in);
+    }
+
+
+    private void readyToSendReceivePacket() {
+        while (true) {
+            try {
+                String clientInput = clientScanner.nextLine();
+                // send a UDP message to server
+                DatagramPacket sent = new DatagramPacket(clientInput.getBytes(), clientInput.getBytes().length, address, port);
+                socket.send(sent);
+
+                // receive a response from server
+                byte[] buffer = new byte[512]; // buffer to receive incoming data from server
+                DatagramPacket received = new DatagramPacket(buffer, buffer.length);
+                socket.receive(received);
+
+                String message = new String(buffer, 0, received.getLength());
+                System.out.println("===== Server: " + message);
+            } catch (SocketException e) {
+                System.err.println("===== Client error: " + e.getMessage());
+                e.printStackTrace();
+            } catch (UnknownHostException e) {
+                System.err.println("===== Client error: " + e.getMessage());
+                e.printStackTrace();
+            } catch (IOException e) {
+                System.err.println("===== Client error: " + e.getMessage());
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        if (args.length < 2) {
             System.out.println("===== Syntax: ClientUDP <IP> <port>");
             return;
         }
 
         String ip = args[0];
-
         int port = Integer.parseInt(args[1]);
-
-        try {
-            InetAddress address = InetAddress.getByName(ip);
-            DatagramSocket socket = new DatagramSocket();
-
-            while (true) {
-                DatagramPacket sent = new DatagramPacket(new byte[1], 1, address, port);
-                socket.send(sent);
-
-                byte[] buffer = new byte[512];
-                DatagramPacket received = new DatagramPacket(buffer, buffer.length);
-                socket.receive(received);
-
-                String message = new String(buffer, 0, received.getLength());
-
-                System.out.println("===== Server: " + message);
-            }
-        } catch (SocketException e) {
-            System.err.println("===== Client error: " + e.getMessage());
-            e.printStackTrace();
-        } catch (UnknownHostException e) {
-            System.err.println("===== Client error: " + e.getMessage());
-            e.printStackTrace();
-        } catch (IOException e) {
-            System.err.println("===== Client error: " + e.getMessage());
-            e.printStackTrace();
-        }
+        ClientUDP client = new ClientUDP(ip, port);
+        System.out.println("===== Running UDP client at IP address of : " + ip);
+        client.readyToSendReceivePacket();
     }
 }
--- project1/src/ServerUDP.java
+++ project1/src/ServerUDP.java
@@ -1,8 +1,9 @@
+import java.io.IOException;
+import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.net.InetAddress;
 import java.net.SocketException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
+import java.util.*;
 
 /**
  * The ServerUDP demonstrates a UDP server program.
@@ -12,9 +13,85 @@ public class ServerUDP {
     private DatagramSocket socket;
     private List<String> listReceived = new ArrayList<String>();
     private Random random;
+    private Map<String, String> store = new HashMap<>();
 
-    public ServerUDP(int port) throws SocketException{
-        socket = new DatagramSocket(port);
+    public ServerUDP() throws SocketException{
+        socket = new DatagramSocket();
+        random = new Random();
+    }
+
+    public static void main(String[] args){
+        try{
+            ServerUDP server = new ServerUDP();
+            server.receiveRequest();
+            server.service();
+        } catch (SocketException e){
+            System.err.println("Socket error: " + e.getMessage());
+        } catch (IOException e){
+            System.err.println("I/O error: " + e.getMessage());
+        }
+    }
+
+    private void service() throws IOException{
+        while(true){
+            // receive a request from client
+            byte[] buffer = new byte[512]; // buffer to receive incoming request from client
+            DatagramPacket request = new DatagramPacket(buffer, buffer.length);
+            socket.receive(request);
+            String sentence = new String(request.getData());
+            String[] requestArr = sentence.split(" ");
+
+            // send response to client
+            InetAddress clientAddress = request.getAddress();
+            int clientPort = request.getPort();
+            keyValService(requestArr, clientAddress, clientPort);
+        }
+    }
+
+    private void keyValService(String[] requestArr, InetAddress clientAddress, int clientPort) throws IOException {
+        while (requestArr.length < 2){
+            String errMsg = "Syntax: <operation> <parameter1>. For example: get apple";
+            sendResult(errMsg, clientAddress, clientPort);
+        }
+        String action = requestArr[0];
+        String key = requestArr[0];
+        if (requestArr.length == 3){
+            while (!action.equals("put")){
+                String errMsg = "For operation of PUT, syntax: <operation> <parameter1> <parameter2>. For example: put apple 10.";
+                sendResult(errMsg, clientAddress, clientPort);
+            }
+        }
+
+
+        switch(action) {
+            case "get":
+                String price = store.get(key);
+                sendResult(price, clientAddress, clientPort);
+                break;
+            case "delete":
+                if (!store.containsKey(key)) {
+                    String errMsg = "Sorry! Store does not carry " + action;
+                    sendResult(errMsg, clientAddress, clientPort);
+                }
+                break;
+            case "put":
+                if (requestArr.length == 3) {
+                    store.put(key, requestArr[2]);
+                    break;
+                }
+
+                String errMsg = "For operation of PUT, syntax: <operation> <parameter1> <parameter2>. For example: put apple 10.";
+                sendResult(errMsg, clientAddress, clientPort);
+                break;
+            default:
+                errMsg = "No operation of " + action;
+                sendResult(errMsg, clientAddress, clientPort);
+        }
+    }
+
+    private void sendResult(String res, InetAddress clientAddress, int clientPort) throws IOException {
+        DatagramPacket response = new DatagramPacket(res.getBytes(), res.getBytes().length, clientAddress, clientPort);
+        socket.send(response);
     }
 
 }
